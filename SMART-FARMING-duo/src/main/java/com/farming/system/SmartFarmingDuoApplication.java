f081a5efb8c6169bf4f4bc91bf3887ea393b3ba7
Poojitha <220101120194@cutm.ac.in>
2024-09-20 11:15:25 +0530

    intial commit

---
+++ SMART-FARMING-duo/src/main/java/com/farming/system/SmartFarmingDuoApplication.java
@@ -0,0 +1,13 @@
+package com.farming.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmingDuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFarmingDuoApplication.class, args);
+	}
+
+}
