3c4ce076e6558b8e49b8a95b5ed0ee492a590af6
Urlana Poojitha <220101120194@cutm.ac.in>
2025-03-31 21:47:34 +0530

    modify the sensordata contr,repo,serv

--- SMART-FARMING-duo/src/main/java/com/farming/system/Repository/SensorDataRepository.java
+++ SMART-FARMING-duo/src/main/java/com/farming/system/Repository/SensorDataRepository.java
@@ -1,10 +1,23 @@
 package com.farming.system.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.farming.system.Model.SensorData;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
 @Repository
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
+	   SensorData findTopByOrderByIdDesc();
+	    Page<SensorData> findAll(Pageable pageable);
+	    List<SensorData> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
+	    
+
 }
--- SMART-FARMING-duo/src/main/java/com/farming/system/Service/SensorDataService.java
+++ SMART-FARMING-duo/src/main/java/com/farming/system/Service/SensorDataService.java
@@ -1,27 +1,40 @@
 package com.farming.system.Service;
 
-
-import org.springframework.stereotype.Service;
-
 import com.farming.system.Model.SensorData;
 import com.farming.system.Repository.SensorDataRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SensorDataService {
 
     private final SensorDataRepository sensorDataRepository;
 
+    @Autowired
     public SensorDataService(SensorDataRepository sensorDataRepository) {
         this.sensorDataRepository = sensorDataRepository;
     }
 
-    public SensorData saveSensorData(SensorData sensorData) {
-        return sensorDataRepository.save(sensorData);
+    public void saveSensorData(SensorData sensorData) {
+        sensorDataRepository.save(sensorData);
     }
 
-    public List<SensorData> getAllSensorData() {
-        return sensorDataRepository.findAll();
+    public SensorData getLatestSensorData() {
+        return sensorDataRepository.findTopByOrderByIdDesc();
+    }
+
+    public Page<SensorData> getAllSensorData(Pageable pageable) {
+        return sensorDataRepository.findAll(pageable);
+    }
+    
+ // Method to get data for a specific time period
+    public List<SensorData> getSensorDataByTimePeriod(LocalDateTime start, LocalDateTime end) {
+        return sensorDataRepository.findByTimestampBetween(start, end);
     }
 }
